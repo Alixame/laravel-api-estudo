7ac0bf608daa0f8ce00a4d9101e5cc90cfbcc9b9
Lucas Alixame <lucasali2003@gmail.com>
2022-03-28 22:32:08 -0300

    [ADD] Material

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credenciais = $request->all('email','password');
+
+        $token = auth('api')->attempt($credenciais);
+
+        if ($token) {
+
+            return response()->json(['mensagem' => "Login efetuado com sucesso!", 'token' => $token],200);
+
+        } else {
+
+            return response()->json(['erro' => "Erro ao efetuar o login"], 403);
+
+        }
+
+    }
+
+    public function logout()
+    {
+        auth('api')->logout();
+        return response()->json(["mensagem" => "Logout efetuado com sucesso!"]);
+    }
+
+    public function refresh()
+    {
+        $token = auth('api')->refresh();
+
+        return response()->json(["token" => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('cliente', 'ClienteController');
-Route::apiResource('carro', 'CarroController');
-Route::apiResource('marca', 'MarcaController');
-Route::apiResource('modelo', 'ModeloController');
+Route::prefix('v1')->middleware('jwt.auth')->group(function () {
+    Route::post('logout', 'AuthController@logout');
+    Route::post('me', 'AuthController@me');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+    Route::apiResource('locacao', 'LocacaoController');
+});
 
-Route::apiResource('locacao', 'LocacaoController');
+
+Route::post('login', 'AuthController@login');
+
+
